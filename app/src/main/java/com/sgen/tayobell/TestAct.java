59ca774535072f7138b5bbbe1c160be9771aec6b
kimmihyeon <alqhd2712@naver.com>
2015-12-22 19:52:30 +0900

    미현-테스트

--- app/src/main/java/com/sgen/tayobell/TestAct.java
+++ app/src/main/java/com/sgen/tayobell/TestAct.java
@@ -4,4 +4,5 @@ package com.sgen.tayobell;
  * Created by Boram on 2015-12-22.
  */
 public class TestAct {
+
 }
--- app/src/main/java/com/sgen/tayobell/ThirdFragment.java
+++ app/src/main/java/com/sgen/tayobell/ThirdFragment.java
@@ -2,6 +2,7 @@ package com.sgen.tayobell;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,7 @@ public class ThirdFragment extends Fragment  {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View V = inflater.inflate(R.layout.tab3_view, container, false);
-
+        Log.d("Test", "성공!!");
         return V;
     }
 }
